0aadb185fa810ac50d99e1c67cab3dc83c884f1b
unknown <shyam.steyp@gmail.com>
2022-01-02 16:47:28 +0530

    contact API fetched

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import './App.css';
-import Login from '../src/components/screens/Login';
-import Dashboard from '../src/components/screens/Dashboard';
-import { BrowserRouter as Router , Route ,Switch } from "react-router-dom"
-import Main from '../src/components/include/Main'
-import Contact from './components/screens/Contact';
-import Single from './components/screens/Single';
-import React from 'react';
+import "./App.css";
+import Login from "../src/components/screens/Login";
+import Dashboard from "../src/components/screens/Dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "../src/components/include/Main";
+import Contact from "./components/screens/Contact";
+import Single from "./components/screens/Single";
+import React, { useState, useEffect } from "react";
 
-export const UserContext = React.createContext()
+export const UserContext = React.createContext();
 
 function App() {
-  return( 
-    <Router>
-  <>  
-    <Switch>
-      <Route path = "/" exact component={Login}/>
-      <Route path = "/dashboard"  component={Dashboard} />
-      <Route path = "/contact"  component={Contact} />
-      <Route path = "/single"  component={Single} />
-    </Switch>
-  </>
-  </Router>
- 
-  )
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setUserData(JSON.parse(localStorage.getItem("user_data")));
+    setLoading(false);
+  }, []);
+
+
+  return loading ? (
+    <h1>Loading</h1>
+  ) : (
+    <>
+      <UserContext.Provider value={{userData}}>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/single" component={Single} />
+          </Switch>
+        </Router>
+      </UserContext.Provider>
+    </>
+  );
 }
 
 export default App;
--- src/components/screens/Contact.js
+++ src/components/screens/Contact.js
@@ -1,51 +1,59 @@
-import React, { useState,useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styled from "styled-components";
-import Main from '../include/Main';
-import user from '../assets/images/image4.svg'
+import Main from "../include/Main";
+import user from "../assets/images/image4.svg";
 import axios from "axios";
-import {Link} from 'react-router-dom';
-import {UserContext} from "../../App"
+import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
 
+export default function Contact({ match }) {
+  const [users, setUsers] = useState([]);
+  const { userData } = useContext(UserContext);
 
-export default function Contact() {
-    const[users, setUsers] = useState([]);
+  useEffect(() => {
+    console.log(userData.data);
+    axios
+      .get(`http://saaskit.tegain.com/api/contact/?page=2`, {
+        headers: {
+          Authorization: `Bearer ${userData.data.access}`,
+        },
+      })
+      .then((response) => {
+          setUsers(response.data.data.results);
+      })
 
-    
+      .catch((error) => {
+        console.log(error);
+      });
 
-    useEffect(() => { 
-        axios.get( "http://saaskit.tegain.com/api/contact/" )
-            .then(function(response){
-                console.log(response);
-                setUsers(response.data.data)
-                console.log("war",users);
-            })
-            .catch(function(error){
-                console.log(error);
-            })
-        },[])
+  }, []);
 
-        const renderItems = () =>{
-            return users.map((user) => (
-                <>
-            <LiList><Input type="checkbox" name="select" /><ImageContainer><Image src={user} alt="Image" /></ImageContainer> Listndey Stroud</LiList>
-            <LiList>fg@example.com</LiList>
-            <LiList>Hatchbuck</LiList>
-            <LiList>Manager</LiList>
-            <LiList>50%</LiList>
-            <LiList>5 minutes</LiList>
-                </>
-            ))
-        }
+  console.log(users);
+  let renderItems = () => {
+    return(
+       users.map((user)=> 
+      <>
+      <UlList >
+        <LiList><Input type="checkbox" name="select" /></LiList>
+        <LiList to="/single">{user.name}</LiList>
+        <LiList>{user.email}</LiList>
+        <LiList>{user.company_name}</LiList>
+        <LiList>{user.role}</LiList>
+        <LiList>{user.forecast}</LiList>
+      </UlList>
+    </>
+      )
+    );
+  };
 
   return (
     <div>
-        
-        <Main/>
+      <Main />
       <Body>
         <Container>
-            <Button>Add Products</Button>
-            <UlList>{renderItems()}</UlList>
-          <UlList>
+          <Button>Add Products</Button>
+          {renderItems()}
+          {/* <UlList>
             <LiList to="/single"><Input type="checkbox" name="select" /><ImageContainer><Image src={user} alt="Image" /></ImageContainer> Listndey Stroud</LiList>
             <LiList>fg@example.com</LiList>
             <LiList>Hatchbuck</LiList>
@@ -84,7 +92,7 @@ export default function Contact() {
             <LiList>Manager</LiList>
             <LiList>50%</LiList>
             <LiList>5 minutes</LiList>
-          </UlList>
+          </UlList> */}
         </Container>
       </Body>
     </div>
@@ -92,49 +100,43 @@ export default function Contact() {
 }
 
 const Body = styled.div`
-    position: relative;
+  position: relative;
 `;
 const Container = styled.div`
-    box-shadow: 9px 6px 46px -24px rgba(0,0,0,0.75);
-    position: absolute;
-    padding: 20px 25px;
-    left: 22%;
-    top: 150px;
-    width: 70%;
+  box-shadow: 9px 6px 46px -24px rgba(0, 0, 0, 0.75);
+  position: absolute;
+  padding: 20px 25px;
+  left: 22%;
+  top: 150px;
+  width: 70%;
 `;
 const Button = styled.button`
-    cursor: pointer;
-    display: block;
-    position: absolute;
-    right: 2%;
-    top:-48px;
-    border:none;
-    padding: 10px 20px;
-    background: #0e9af1;
-    color:#fff;
+  cursor: pointer;
+  display: block;
+  position: absolute;
+  right: 2%;
+  top: -48px;
+  border: none;
+  padding: 10px 20px;
+  background: #0e9af1;
+  color: #fff;
 `;
 const Input = styled.input``;
 const UlList = styled.ul`
-    display: flex;
-    justify-content:space-between;   
-    align-items:center;
-    padding: 20px;
-    border-bottom:1px solid grey;
+  /* display: flex;
+  justify-content: space-between; */
+  align-items: center;
+  padding: 20px;
+  border-bottom: 1px solid grey;
 `;
 const LiList = styled(Link)`
-    &:nth-child(2){
-        display: flex;
-        align-items:center
-    }
-    &:nth-child(1){
-        display: flex;
-        justify-content:space-between;
-    }
+ margin-left: 20px;
+  
 `;
 const ImageContainer = styled.div`
-    margin-right:10px;
-    margin-left:10px;
+  margin-right: 10px;
+  margin-left: 10px;
 `;
 const Image = styled.img`
-    border-radius:50%;
+  border-radius: 50%;
 `;
